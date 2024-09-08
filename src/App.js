2da5ad031443466b3aa614de8f9e2d8db268b9b0
Mirodil Mirzoidov <118127125+Mirzoidov@users.noreply.github.com>
2024-09-08 12:29:25 +0500

    Nav completed static styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import GlobalStyles from "./components/GlobalStyle";
 // import pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
       <GlobalStyles />  {/* Import global styles */}
+      <Nav />
       <AboutUs />
     </div>
   );
