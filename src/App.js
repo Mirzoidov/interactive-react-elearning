c308e51b0ee54f8c5204c710bd95dfe429caffec
Mirodil Mirzoidov <118127125+Mirzoidov@users.noreply.github.com>
2024-09-08 10:57:33 +0500

    Styled Component Intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+// Global styles
+import GlobalStyles from "./components/GlobalStyle";
 // import pages
 import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
-      <h1>eLearning</h1>
+      <GlobalStyles />  {/* Import global styles */}
       <AboutUs />
     </div>
   );
